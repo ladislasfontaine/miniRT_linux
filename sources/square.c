b779a417c549456e23d9bc96cbd818c78ba19357
ladislasfontaine <fontaine.ladislas@gmail.com>
2020-05-11 10:21:39 +0200

    added 42 headers

--- sources/square.c
+++ sources/square.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   square.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lafontai <lafontai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/11 10:20:55 by lafontai          #+#    #+#             */
+/*   Updated: 2020/05/11 10:20:57 by lafontai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
 /*
@@ -18,10 +30,11 @@ int		square_intersection(t_intersec *intersec, t_shape *square)
 	float		t_min;
 	float		t_max;
 	float		temp;
-
+	
 	v_min = init_vector(square->center->x - (square->side / 2.0), square->center->y - (square->side / 2.0), square->center->z - (square->side / 2.0));
 	v_max = init_vector(square->center->x + (square->side / 2.0), square->center->y + (square->side / 2.0), square->center->z + (square->side / 2.0));
-	//v_min->x == B0x dans le cours
+	// v_min->x == B0x dans le cours
+	// 6 t car il y a 6 planes pour faire un carré
 	t0x = (v_min->x - intersec->ray->origin->x) / intersec->ray->direction->x;
 	t0y = (v_min->y - intersec->ray->origin->y) / intersec->ray->direction->y;
 	t0z = (v_min->z - intersec->ray->origin->z) / intersec->ray->direction->z;
@@ -53,7 +66,7 @@ int		square_intersection(t_intersec *intersec, t_shape *square)
 	t_max = (t1x < t1y) ? t1x : t1y;
 	t_max = (t_max < t1z) ? t_max : t1z;
 
-	if (t0x > t1y || t0y > t1x || t0x > t1z || t0y > t1z || t0z > t1y || t0z > t1x) 
+	if (t_min > t_max)//t0x > t1y || t0y > t1x || t0x > t1z || t0y > t1z || t0z > t1y || t0z > t1x) 
     	return (-1);
 	if (t_min > RAY_MIN && t_min < intersec->t)
 		intersec->t = t_min;
@@ -62,7 +75,7 @@ int		square_intersection(t_intersec *intersec, t_shape *square)
 	else
 		return (0);
 	intersec->shape = square;
-	intersec->normal = init_vector(-1, 0, 0);
+	intersec->normal = init_vector(0, 1, 0);
 
 	return (0);
 }
